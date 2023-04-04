63a3c01359eab5d48b0949e0eaa5f62cee461f83
Maartala <Steffi.Gerstenmaier@t-online.de>
2023-04-04 16:58:59 +0200

    second upload

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-console.log("bla");
-
 const davor = document.getElementById('davor');
 const danach = document.getElementById('danach');
-
-const button = document.getElementById('button');
 
 let outputVorne = document.getElementById('outputVordererTeil');
 let outputHinten = document.getElementById('outputHintererTeil');
@@ -13,24 +9,15 @@ const trennen = () =>{
     let ganzerSatz = document.getElementById('inputSentence').value;
     let trennungsWort = document.getElementById('inputTrennungsWort').value;
 
-    let ersterTeil = "";
-    let zweiterTeil = "";
-
-
     if (davor.checked) {
-        // console.log(ganzerSatz.indexOf(trennungsWort));
-        // console.log(ganzerSatz.slice(ganzerSatz.indexOf(trennungsWort)));
-
         outputHinten.innerHTML = ganzerSatz.slice(ganzerSatz.indexOf(trennungsWort))
         outputVorne.innerHTML = ganzerSatz.slice(0, (ganzerSatz.indexOf(trennungsWort)))
 
     } else if (danach.checked){
-
         outputHinten.innerHTML = ganzerSatz.split(trennungsWort)[1];
-
         outputVorne.innerHTML = (ganzerSatz.slice(0, (ganzerSatz.indexOf(trennungsWort)))) + trennungsWort
     }
     else {
-        console.log("Radio-Button muss ausgewählt sein");
+        console.log("Ein Radio-Button muss ausgewählt sein");
     }
 }
